44a1324ba94e5c4bbf7f9af97089e18b1cfb2107
zoton2 <zoton2@gmail.com>
2019-07-30 19:19:24 +0200

    Actually initilise the routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Config, loadConfig } from "./config";
 import { HTTPServer } from "./http";
 import { MQ } from "./mq";
 
-const config: Config = loadConfig()
-const mq = new MQ(config)
+const config: Config = loadConfig();
+const mq = new MQ(config);
 const http = new HTTPServer(config, mq);
+http.initRoutes();
