e3ad1be8633b03c2a5ecd35781c618155f13e026
zoton2 <zoton2@gmail.com>
2020-04-02 21:43:47 +0100

    Replace tslint with eslint, bump everything, other development related tweaking

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        moduleDirectory: [
+          'node_modules',
+          '.',
+        ],
+      },
+    },
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
+    'max-len': ['error', { 'code': 100 }],
+    // 'no-console': 'off',
+    // 'lines-between-class-members': 'off',
+  },
+};
