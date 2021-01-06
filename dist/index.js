03e3fccedff010fe3b4a6d4e512a6fa11044f683
zoton2 <zoton2@gmail.com>
2021-01-06 01:09:26 +0000

    Remove built files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config_1 = require("./config");
-var http_1 = require("./http");
-var mq_1 = require("./mq");
-var config = config_1.loadConfig();
-var mq = new mq_1.MQ(config);
-var http = new http_1.HTTPServer(config, mq);
-http.initRoutes();
